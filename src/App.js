191c5289f0528273845b79f34ef97298c3121319
renebreslauer <renebreslauer@me.com>
2021-01-14 09:45:23 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import Hero from './components/Hero'
 import Footer from './components/Footer'
+import {
+  BrowserRouter,
+  Route,
+  Link,
+  Redirect,
+  withRouter,
+} from 'react-router-dom'
 import './App.scss'
 
 function App() {
